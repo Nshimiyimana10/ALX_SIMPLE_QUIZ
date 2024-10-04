67c48bc9289ffcb201c5133274856686d4c21849
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-04 12:21:38 -0700

    added

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
 const choice1 = document.getElementById('choice1');
 const choice2 = document.getElementById('choice2');
 const choice3 = document.getElementById('choice3');
+const button = document.getElementById('submit-answer')
