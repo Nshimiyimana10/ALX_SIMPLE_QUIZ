a74710d33366d3f7988438c45eada8c372bc976c
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-04 12:18:23 -0700

    added js again

--- quiz.js
+++ quiz.js
@@ -1,1 +1,3 @@
-const choice1 = document.getElementById('choice1')
+const choice1 = document.getElementById('choice1');
+const choice2 = document.getElementById('choice2');
+const choice3 = document.getElementById('choice3');
