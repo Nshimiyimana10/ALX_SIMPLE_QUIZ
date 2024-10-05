e3b0fef337f51f530e2aef51cff32bc3495cf87d
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-05 08:04:34 -0700

    added new js

--- quiz.js
+++ quiz.js
@@ -1,8 +1,28 @@
 const choice1 = document.querySelector('#choice1');
 const choice2 = document.querySelector('#choice2');
 const choice3 = document.querySelector('#choice3');
-const button = document.getElementById('submit-answer')
+const button = document.getElementById('submit-answer');
+let feedback = document.getElementById("feedback");
 
 function checkAnswer(){
     let correctAnswer = "4";
+    let userAnswer = ""
+    if(choice1.checked && userAnswer ==correctAnswer){
+        feedback.textContent = "Correct! Well done."
+    }
+
+    else if(choice2.checked && userAnswer ==correctAnswer){
+        feedback.textContent = "Correct! Well done."
+
+    }
+
+    else if(choice3.checked && userAnswer ==correctAnswer){
+        feedback.textContent = "Correct! Well done."
+
+    }
+
+    else{
+        feedback.textContent = "That's incorrect. Try again!"
+    }
+
 }
