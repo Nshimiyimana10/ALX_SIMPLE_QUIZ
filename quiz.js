f90797b3a478d7bd899a94829a2d34f4586e5f6a
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-05 07:55:09 -0700

    added

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
-const choice1 = document.getElementById('choice1');
-const choice2 = document.getElementById('choice2');
-const choice3 = document.getElementById('choice3');
+const choice1 = document.querySelector('#choice1');
+const choice2 = document.querySelector('#choice2');
+const choice3 = document.querySelector('#choice3');
 const button = document.getElementById('submit-answer')
 
 function checkAnswer(){
