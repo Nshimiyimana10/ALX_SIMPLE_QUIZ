ed50ca70e73f149d87166d6b409b6c937547d724
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-05 08:06:37 -0700

    added

--- quiz.js
+++ quiz.js
@@ -1,8 +1,9 @@
 const choice1 = document.querySelector('#choice1');
 const choice2 = document.querySelector('#choice2');
 const choice3 = document.querySelector('#choice3');
-const button = document.getElementById('submit-answer');
 let feedback = document.getElementById("feedback");
+
+const button = document.getElementById('submit-answer').addEventListener("click", checkAnswer)
 
 function checkAnswer(){
     let correctAnswer = "4";
