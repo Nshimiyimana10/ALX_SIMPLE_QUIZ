56e338855865270ae07932187e28b853e3cd8f76
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-06 02:21:26 -0700

    added

--- quiz.js
+++ quiz.js
@@ -3,23 +3,15 @@ const choice2 = document.querySelector('#choice2');
 const choice3 = document.querySelector('#choice3');
 const feedback = document.getElementById("feedback");
 const choice = document.querySelector("quiz");
-const userAnswer = document.querySelectorAll('input');
+const userAnswer = document.querySelector('input').checked;
 console.log(userAnswer);
 
 const button = document.getElementById('submit-answer').addEventListener("click", checkAnswer)
 
 function checkAnswer(){
     let correctAnswer = "4";
-    if(userAnswer[0].value.checked ===correctAnswer){
+    if(userAnswer===correctAnswer){
         feedback.textContent = "Correct! Well done."
-    }
-
-    else if(userAnswer[1].value.checked === correctAnswer){
-        feedback.textContent === "correct! Well done"
-    }
-
-    else if(userAnswer[2].value.checked === correctAnswer){
-        feedback.textContent === "correct! Well done"
     }
 
 
